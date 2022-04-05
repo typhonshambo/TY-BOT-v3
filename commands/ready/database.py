5018d325dd81de254f38b5bee2a5bcab29ea4af9
typhonshambo <54593764+typhonshambo@users.noreply.github.com>
2022-04-05 22:59:22 +0530

    Update database.py

--- commands/ready/database.py
+++ commands/ready/database.py
@@ -22,13 +22,6 @@ class database(commands.Cog):
 		print("[\] DATABASE READY")
 
 		
-	@commands.Cog.listener() 
-	async def on_member_join(self, member):
-		background= "https://raw.githubusercontent.com/typhonshambo/TY-BOT-v3/main/assets/levelBG1.png"
-		canvas = "#E9DAC7"
-		member_id = str(member.id)
-		guild_id = str(member.id) 
-		await self.bot.pg_con.execute("UPDATE users SET background = $1, color = $2 WHERE user_id = $3 AND guild_id = $4",background, str(canvas) ,member_id, guild_id)
 
 		
 def setup(bot):
