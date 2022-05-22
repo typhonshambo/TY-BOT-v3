dd2f7e18dfcdd3418f27e93f51eeeed3782d0621
Shambo Chowdhury <54593764+typhonshambo@users.noreply.github.com>
2022-05-22 12:49:28 +0530

    included logger

---
+++ commands/logger/invites.py
@@ -0,0 +1,37 @@
+import discord
+from discord.ext import commands
+from discord import Embed, Webhook
+import DiscordUtils 
+import aiohttp
+import os
+
+webhookUrl = os.environ.get('inviteLogWebhookUrl')
+
+class inviteLog(commands.Cog):
+	def __init__(self, bot):
+		self.bot = bot
+		
+
+	@commands.Cog.listener() 
+	async def on_member_join(self, member):
+
+		tracker = DiscordUtils.InviteTracker(self.bot)
+		inviter = await tracker.fetch_inviter(member)
+		embed = discord.Embed(
+			color = discord.Color.random(),
+			timestamp=discord.utils.utcnow(),
+			description = f"""
+			> {inviter} invited {member.mention}
+			"""
+		)
+		embed.set_thumbnail(url=member.display_avatar.url)
+
+		async def foo():
+			async with aiohttp.ClientSession() as session:
+				webhook = Webhook.from_url(webhookUrl, session=session)
+				await webhook.send(embed=embed, username='invites', avatar_url="https://media.discordapp.net/attachments/869155012809474068/959039088927838228/unknown.png?width=326&height=326")
+		await foo()
+
+
+def setup(bot):
+	bot.add_cog(inviteLog(bot))
